d82c872e6445b963f0bef618c65a0d2d4be27f64
elibiz443 <ellyambet10@gmail.com>
2023-04-20 04:06:44 +0300

    Improved user model test to include shoulda syntax

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
 
   has_secure_password
 
-  validates :email, presence: true, uniqueness: true, format: {with: URI::MailTo::EMAIL_REGEXP}
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, format: {with: URI::MailTo::EMAIL_REGEXP}
   validates :phone_number, presence: true, uniqueness: true
   validates :first_name, presence: true
   validates :last_name, presence: true
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,37 +1,63 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  subject { build(:user) }
+  let(:user) { FactoryBot.create(:user) }
+  let(:token) { JWT.encode({ user_id: user.id }, Rails.application.secret_key_base) }
+
+  describe "Valid FactoryBot" do 
+    it "has a valid factory" do
+      expect(FactoryBot.create(:user)).to be_valid
+    end
+  end
+
+  describe "associations" do
+    it { should have_one(:auth_token).dependent(:destroy) }
+  end
+
   describe "validations" do
-    let(:user) { build(:user) }
+    it { should be_valid }
+    it { should validate_presence_of(:first_name) }
+    it { should validate_presence_of(:last_name) }
+    it { should validate_presence_of(:phone_number) }
+    it { should validate_uniqueness_of(:phone_number) }
+    it { should validate_presence_of(:email) }
+    it { should validate_uniqueness_of(:email).case_insensitive }
+    it { should validate_presence_of(:password) }
+    it { should validate_length_of(:password).is_at_least(6) }
+    it { should validate_presence_of(:password_confirmation) }
+    it { should validate_length_of(:password_confirmation).is_at_least(6) }
+  end
 
-    it "is valid with valid attributes" do
-      expect(user).to be_valid
+  describe "Generating auth_token" do
+    it "creates an auth token for the user" do
+      expect { user.generate_auth_token }.to change { AuthToken.count }.by(1)
+      expect(user.auth_token).to be_present
     end
 
-    it "is not valid without a first name" do
-      user.first_name = nil
-      expect(user).not_to be_valid
+    it "returns a JWT token" do
+      token = user.generate_auth_token
+      expect(token).to be_a(String)
+      expect(token).not_to be_blank
+    end
+  end
+
+  describe "Find By Token" do
+    it "returns the user for a valid token" do
+      expect(User.find_by_token(token)).to eq(user)
     end
 
-    it "is not valid without a last name" do
-      user.last_name = nil
-      expect(user).not_to be_valid
+    it "returns nil for an invalid token" do
+      expect(User.find_by_token("invalid_token")).to be_nil
     end
+  end
 
-    it "is not valid without a phone number" do
-      user.phone_number = nil
-      expect(user).not_to be_valid
-    end
-
-    it "is not valid without a unique email" do
-      existing_user = create(:user)
-      user.email = existing_user.email
-      expect(user).not_to be_valid
-    end
-
-    it "is not valid without a password" do
-      user.password = nil
-      expect(user).not_to be_valid
+  describe "Invalidate token" do
+    it "destroys the user's auth token" do
+      user.generate_auth_token
+      expect { user.invalidate_token }.to change { AuthToken.count }.by(-1)
+      expect(user.auth_token).to be_nil
     end
   end
 end
+
