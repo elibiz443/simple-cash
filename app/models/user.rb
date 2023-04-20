2452dce76a5f79bd2fe74723dda5f7d6c07f9559
elibiz443 <ellyambet10@gmail.com>
2023-04-20 04:36:40 +0300

    Updated user model test to include case insensitivity of phone_number

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :email, presence: true, uniqueness: { case_sensitive: false }, format: {with: URI::MailTo::EMAIL_REGEXP}
-  validates :phone_number, presence: true, uniqueness: true
+  validates :phone_number, presence: true, uniqueness: { case_sensitive: false }
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :password, presence: true, length: { minimum: 6 }
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -20,7 +20,7 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of(:first_name) }
     it { should validate_presence_of(:last_name) }
     it { should validate_presence_of(:phone_number) }
-    it { should validate_uniqueness_of(:phone_number) }
+    it { should validate_uniqueness_of(:phone_number).case_insensitive }
     it { should validate_presence_of(:email) }
     it { should validate_uniqueness_of(:email).case_insensitive }
     it { should validate_presence_of(:password) }
