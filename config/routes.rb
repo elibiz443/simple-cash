dd0a32686da90b6590233853e54e12a6bd1c80ba
elibiz443 <ellyambet10@gmail.com>
2023-04-21 09:46:19 +0300

    Added transactions controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
       post "/login", to: "sessions#create"
       delete "/logout", to: "sessions#destroy"
       resources :top_ups
+      resources :transactions
     end
   end
 end
---
+++ spec/requests/api/v1/transactions_spec.rb
@@ -0,0 +1,6 @@
+require 'rails_helper'
+
+RSpec.describe "Api::V1::Transactions", type: :request do
+  describe "GET /index" do
+  end
+end
