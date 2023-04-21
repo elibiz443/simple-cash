db84f8992989eba3d7e62f1f49bb90ed111f0728
elibiz443 <ellyambet10@gmail.com>
2023-04-21 10:55:41 +0300

    Updated transaction model and added detail to transaction controller

--- app/controllers/api/v1/transactions_controller.rb
+++ app/controllers/api/v1/transactions_controller.rb
@@ -4,14 +4,21 @@ class Api::V1::TransactionsController < ApplicationController
   def create
     amount = params[:amount].to_f
     phone_number = params[:phone_number]
-    @top_up = TopUp.new(amount: amount, phone_number: phone_number)
+    email = params[:email]
+    user_id = @current_user.id
 
-    if @top_up.save
-      user = User.find_by(phone_number: phone_number)
-      user.update(balance: (user.balance + @top_up.amount))
-      render json: { message: "Top Up Successful 👍", current_balance: user.balance }, status: :created
+    @transaction = Transaction.new(amount: amount, phone_number: phone_number, email: email, user_id: user_id)
+
+    if @transaction.save
+      current_user = User.find_by_id(@transaction.user_id)
+      recipient = User.find_by(phone_number: phone_number) || User.find_by(email: email)
+
+      current_user.update(balance: (current_user.balance - @transaction.amount))
+      recipient.update(balance: (recipient.balance + @transaction.amount))
+
+      render json: { message: "Transaction Successful 👍", user_balance: current_user.balance, recipient_balance: recipient.balance }, status: :created
     else
-      render json: { errors: @top_up.errors.full_messages }, status: :unprocessable_entity
+      render json: { errors: @transaction.errors.full_messages }, status: :unprocessable_entity
     end
   end
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,12 +1,30 @@
 class Transaction < ApplicationRecord
   before_create :capture_sending_time
+  before_create :verify_recipient_user
+  before_create :verify_amount
 
   belongs_to :user
-  validates :amount, presence: true
+
+  validates :amount, presence: true, numericality: { greater_than: 0, message: "must be present or greater than 0❗" }
   validates :phone_number, presence: true, :numericality => true, :length => { :minimum => 9, :maximum => 13 }, unless: :email
   validates :email, presence: true, format: {with: URI::MailTo::EMAIL_REGEXP}, unless: :phone_number
 
   def capture_sending_time
     self.sending_time = Time.now
+  end
+
+  def verify_recipient_user
+    unless User.exists?(phone_number: self.phone_number) || User.exists?(email: self.email)
+      errors.add(:phone_number_or_email, "does not exist 🚫")
+      throw(:abort)
+    end
+  end
+
+  def verify_amount
+    current_balance = User.find_by_id(self.user_id).balance
+    unless current_balance > self.amount
+      errors.add(:amount, "more than the existing balance ❌")
+      throw(:abort)
+    end
   end
 end
