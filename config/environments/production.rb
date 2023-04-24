4b5502cb95ee70676e6fe229f88fcd5ce199f59a
elibiz443 <ellyambet10@gmail.com>
2023-04-24 23:46:03 +0300

    Updated mailer configuration in development and production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,21 +1,6 @@
 require "active_support/core_ext/integer/time"
 
-Rails.application.configure do
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { :host => "simplecash.herokuapp.com", protocol: "http" }
-
-  my_user_name = "ellyambet10@gmail.com"
-  my_password = "srvklnwqmofnvguy"
-
-  config.action_mailer.smtp_settings = {
-    :address              => "smtp.gmail.com",
-    :port                 => 587,
-    :user_name            => my_user_name,
-    :password             => my_password,
-    :authentication       => "plain",
-    :enable_starttls_auto => true
-  }
-    
+Rails.application.configure do  
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
@@ -75,6 +60,21 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => "simplecash.herokuapp.com", protocol: "http" }
+
+  my_user_name = "ellyambet10@gmail.com"
+  my_password = "srvklnwqmofnvguy"
+
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :user_name            => my_user_name,
+    :password             => my_password,
+    :authentication       => "plain",
+    :enable_starttls_auto => true
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
