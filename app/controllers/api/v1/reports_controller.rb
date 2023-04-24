35951c7997c5ae15e1400ccf33188cff9247d987
elibiz443 <ellyambet10@gmail.com>
2023-04-24 21:17:29 +0300

    Updating report

--- app/controllers/api/v1/reports_controller.rb
+++ app/controllers/api/v1/reports_controller.rb
@@ -11,8 +11,7 @@ class Api::V1::ReportsController < ApplicationController
     @report = Report.new(start_date: start_date, end_date: end_date, user_id: user_id)
 
     if @report.save
-      # @transactions = Transaction.where(user_id: @report.user_id, created_at: @report.start_date..@report.end_date)
-      @transactions = Transaction.where(user_id: 3, created_at: "2023-04-12T06:43:33.997064000-04:00".."2023-04-24T07:43:33.997064000-04:00")
+      @transactions = Transaction.where(user_id: @report.user_id, created_at: @report.start_date..@report.end_date)
       render json: { message: "Report Creation Successful 👍", report: @report, transactions: @transactions }, status: :created
     else
       render json: { errors: @report.errors.full_messages }, status: :unprocessable_entity
